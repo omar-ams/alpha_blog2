3948c4a897df91cfd29b7589d1c4b71bef3f0eca
protege <o.amous@yahoo.com>
2025-09-17 20:28:45 +0100

    pg

--- Gemfile
+++ Gemfile
@@ -59,6 +59,12 @@ group :development do
   # gem "spring"
 end
 
+group :production do 
+
+  gem "pg"
+
+end
+
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @article = Article.all
   end
@@ -9,7 +11,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
 
   end
 
@@ -24,7 +25,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-      @article = Article.find(params[:id])
       if @article.update(article_params)
         flash[:notice] = "Article modifié"
       redirect_to @article
@@ -34,10 +34,8 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 def destroy
-  @article = Article.find(params[:id]) 
   @article.destroy
   flash[:notice] = "Article supprimé"
   redirect_to articles_path
@@ -46,6 +44,10 @@ end
 
 
   private
+
+  def set_article
+ @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
