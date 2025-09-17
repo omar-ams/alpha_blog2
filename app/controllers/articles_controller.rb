76baa399062a48ed5d5c6fdec8ffb1ec0e668b0e
protege <o.amous@yahoo.com>
2025-09-17 19:58:49 +0100

    Destroy action et some partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,14 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+def destroy
+  @article = Article.find(params[:id]) 
+  @article.destroy
+  flash[:notice] = "Article supprimé"
+  redirect_to articles_path
+end
+
+
 
   private
 
